289873d8aac466b0fc24b0db185c90167e1b3b46
Tomas Agustin Lopez <111098118+tomas-lp@users.noreply.github.com>
2024-12-14 12:49:25 -0300

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,37 +7,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        light: {
-          border: '#E2E8F0',
-          blue: '#98BEF6',
-          cyan: '#A6F6E8',
-          pink: '#F592D8',
-          yellow: '#FDFD94',
-          green: '#94FB80',
-          peach: '#F9CEA4',
-          purple: '#B1AEFC',
-          red: '#FF9898',
-          primary: '#000000',
-          secondary: '#383838'
-        },
-        dark: {
-          blue: '#306CC7',
-          cyan: '#40B19B',
-          pink: '#D847AC',
-          yellow: '#B4B42E',
-          green: '#3CB85D',
-          peach: '#CE8C4A',
-          purple: '#625DDE',
-          red: '#C54444',
-          background: '#121212',
-          border: '#2D2F32',
-          primary: '#FFFFFF',
-          secondary: '#D1D1D1'
-        },
-        disabled: '#8E8E8E',
-        danger: '#FF7878',
-        success: '#66D857',
-        warning: '#FFFF2C'
+        border: 'var(--border-color)',
+        blue: 'var(--blue-color)',
+        cyan: 'var(--cyan-color)',
+        pink: 'var(--pink-color)',
+        yellow: 'var(--yellow-color)',
+        green: 'var(--green-color)',
+        peach: 'var(--peach-color)',
+        purple: 'var(--purple-color)',
+        red: 'var(--red-color)',
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+        background: 'var(--background-color)',
+        danger: 'var(--danger-color)',
+        success: 'var(--success-color)',
+        warning: 'var(--warning-color)',
+        disabled: 'var(--disabled-color)'
       }
     }
   },
