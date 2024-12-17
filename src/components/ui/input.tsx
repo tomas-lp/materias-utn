ff8bdc7b100bc962468b4fb3f5aa6ef0e3337976
Tomas Agustin Lopez <111098118+tomas-lp@users.noreply.github.com>
2024-12-17 16:03:43 -0300

    Fix inputs

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,6 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
+        autoComplete='off'
         type={type}
         className={cn(
           'flex h-10 w-full rounded-md border border-input bg-app-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
@@ -28,6 +29,7 @@ const SearchInput = React.forwardRef<
   <div className='flex items-center w-full rounded-md border border-input bg-app-border px-3 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm'>
     <Search className='mr-2 h-4 w-4 shrink-0 opacity-50' />
     <input
+      autoComplete='off'
       className={cn(
         'w-full py-3 bg-transparent outline-none',
         className
